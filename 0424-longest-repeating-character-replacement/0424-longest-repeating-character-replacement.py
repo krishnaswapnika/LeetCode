4cc1d5d2ef261df9a95a643d4958005a2b98542b
Goriparthi krishna Swapnika <142794188+krishnaswapnika@users.noreply.github.com>
2025-06-30 21:26:52 +0530

    Time: 108 ms (87.05%), Space: 18.2 MB (23.36%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,16 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = [0] * 26  # For A-Z
+        max_count = 0
+        l= 0
+        res= 0
+        for r in range(len(s)):
+            index = ord(s[r]) - ord('A')
+            count[index] += 1
+            max_count = max(max_count, count[index])
+            if (r-l+1) - max_count > k:
+                count[ord(s[l]) - ord('A')] -= 1
+                l += 1
+            res = max(res, r-l+ 1)
+        return res
+
