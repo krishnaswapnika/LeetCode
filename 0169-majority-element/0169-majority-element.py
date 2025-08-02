f75e157ce91caf2bb246a4e038278f4c7ad137f7
Goriparthi krishna Swapnika <142794188+krishnaswapnika@users.noreply.github.com>
2025-08-02 21:59:24 +0530

    Time: 5 ms (65.01%), Space: 19.4 MB (19.14%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,15 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        m= len(nums)//2
-        dic = {}
-        for i in range(len(nums)):
-            if nums[i] in dic:
-                dic[nums[i]] += 1
-            else:
-                dic[nums[i]] = 1
-        for key in dic:
-            if dic[key] > m:
-                return key
+        c = 0
+        num = None
+        for i in nums:
+            if c == 0:
+                num = i
+            c += (1 if i==num else -1)
+        return num
 
         
 
