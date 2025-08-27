22e31e037df13db071d27852bdaafebbae71c5f6
Goriparthi krishna Swapnika <142794188+krishnaswapnika@users.noreply.github.com>
2025-08-27 22:42:49 +0530

    Time: 7 ms (25.2%), Space: 17.6 MB (93.1%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,25 +1,25 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        queue = deque()
-        fresh = 0
-        mint = 0
+        m, n= len(grid), len(grid[0])
+        queue =deque()
+        fresh =0
+        mint =0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 2:
+                if grid[i][j] ==2:
                     queue.append((i, j))
-                elif grid[i][j] == 1:
+                elif grid[i][j]== 1:
                     fresh += 1
-        d= [(-1, 0),(1, 0),(0, -1),(0, 1)]
+        d= [(-1,0),(1,0),(0,-1),(0,1)]
         while queue and fresh > 0:
             for _ in range(len(queue)):
                 x, y = queue.popleft()
                 for dx, dy in d:
                     nx, ny = x + dx, y + dy
-                    if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 1:
-                        grid[nx][ny] = 2
-                        fresh -= 1
-                        queue.append((nx, ny))
+                    if 0 <= nx<m and 0<= ny<n and grid[nx][ny] == 1:
+                        grid[nx][ny]= 2
+                        fresh -=1
+                        queue.append((nx,ny))
             if queue:  
                 mint += 1
         return mint if fresh == 0 else -1
